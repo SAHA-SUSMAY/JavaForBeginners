27f8f61beeccfc4b21c5b00edac26d7c2c5f9a30
LAPTOP-PTD4L1GV\susma <susmaysaha4800@gmail.com>
2025-05-26 11:38:01 +0900

    HAPPY

---
+++ src/RectANDoval.java
@@ -0,0 +1,9 @@
+public class RectANDoval extends MyFrame {
+	public void run() {
+		fillRect(25, 130, 100, 15);
+		fillRect(50, 110, 50, 30);
+		fillRect(85, 140, 20, 20);
+		fillRect(45, 140, 20, 20);
+	}
+
+}
