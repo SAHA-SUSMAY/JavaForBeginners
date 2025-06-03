c8a9b267935d55bae801df4e596fdc22f294a286
LAPTOP-PTD4L1GV\susma <susmaysaha4800@gmail.com>
2025-06-03 10:02:15 +0900

    Display

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,16 +1,34 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1=new Face(50,50);
+		Face face1=new Face(50,50,10,5);
 
 		face1.draw(this);
 		
-		Face face2=new Face(200,100);
+		Face face2=new Face(200,100,-10,-5);
 		
 		face2.draw(this);
 		
-		Face face3=new Face(125,150);
-		face3.draw(this);
+
+		
+		for(int i=0;i<30;i++) {
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+			sleep(0.1);
+			}
+			
+			for(int i=0;i<30;i++) {
+				clear();
+				face1.draw(this);
+				face2.draw(this);
+				face1.move();
+				face2.move();
+				
+			
+			
+				sleep(0.1);
+		}
 		
   
 	}
