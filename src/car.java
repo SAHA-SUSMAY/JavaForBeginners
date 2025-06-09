acb29f3533b3169eeb1e5e85f3d4bc0ac35623e6
LAPTOP-PTD4L1GV\susma <susmaysaha4800@gmail.com>
2025-06-09 12:14:35 +0900

    CAR

--- src/car.java
+++ src/car.java
@@ -1,33 +1,28 @@
 
-public class car {
-		int x,y,vx,vy;
-		public car(int x,int y,int vx,int vy) {
-			this.x = x;
-			this.y = y;
-			this.vx = vx;
-			this.vy = vy;
-		}
-	    public void draw(MyFrame frame) {
-	    	frame.fillRect(x,y,40,20);
-			frame.fillRect(x-20,y + 20,80,20);
-			frame.fillOval(x - 10, y + 40,20,20);
-			frame.fillOval(x + 30, y + 40,20,20);
-	    	
-	    }
-	    public void move() {
-	    	x += vx;
-	    	y += vy;
-	    	
-	    	if (x>450) {
-	    		x-=500;
-	    	}
-	    }
-	 
-	
-	 
-		
-		
-		
+public class car extends Vehicle {
+	int x, y, vx, vy;
+
+	public car(int x, int y, int vx, int vy) {
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
 	}
 
+	public void draw(MyFrame frame) {
+		frame.fillRect(x + 20, y, 40, 20);
+		frame.fillRect(x, y + 20, 80, 20);
+		frame.fillOval(x + 10, y + 40, 20, 20);
+		frame.fillOval(x + 50, y + 40, 20, 20);
 
+	}
+
+	public void move() {
+		x += vx;
+		y += vy;
+
+	}
+
+}
