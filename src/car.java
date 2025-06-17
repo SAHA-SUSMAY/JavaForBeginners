faff91e829834b6ba8826fa430e9eb6cfc1c28ac
LAPTOP-PTD4L1GV\susma <susmaysaha4800@gmail.com>
2025-06-17 12:34:26 +0900

    ABCD

--- src/car.java
+++ src/car.java
@@ -1,6 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class car extends Vehicle {
-	int x, y, vx, vy;
+public class car extends Vehicle implements KeyListener {
 
 	public car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
@@ -16,12 +17,41 @@ public class car extends Vehicle {
 		frame.fillRect(x, y + 20, 80, 20);
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
-
 	}
 
-	public void move() {
-		x += vx;
-		y += vy;
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			vy = -2;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			vy = 4;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			vy = 0;
+		}
+	
+	if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+		vy = 0;
+	}
+	}
+
+	public void keyTyped(KeyEvent e) {
 
 	}
 
